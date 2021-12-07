0ed0cbe5a67ca69302bc549abd4de9d383809831
rxn40 <yvette.groner@uni-ulm.de>
2021-12-07 20:00:24 +0100

    Datenbank begonnen aufzuräumen -> update fehlt noch

---
+++ app/src/main/java/com/example/haushaltsapp/ToDoListPackage/TaskModel.java
@@ -0,0 +1,32 @@
+package com.example.haushaltsapp.ToDoListPackage;
+
+//Aufbau einer Task / To Do Models
+
+public class TaskModel {
+    private int id, status;
+    private String task;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+}
