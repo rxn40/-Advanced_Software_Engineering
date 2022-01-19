c09f9d08d6e3dbf54cd8cdab14b49dbf032fe796
rxn40 <yvette.groner@uni-ulm.de>
2022-01-19 19:40:09 +0100

    l

--- app/src/main/java/com/example/haushaltsapp/ToDoListPackage/ToDoInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.haushaltsapp.ToDoListPackage;
-
-import android.content.DialogInterface;
-
-public interface ToDoInterface {
-    public void handleDialogClose(DialogInterface dialog);
-    public void onTaskClick(int position);
-}
